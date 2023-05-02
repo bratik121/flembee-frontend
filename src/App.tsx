b52664a6456bc4da1bbf722436a07eb40334a1d0
dimeBrii <thiswasbrii@gmail.com>
2023-05-02 02:58:09 -0400

    anadiendo rutas y renderizados condicionales

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Tasks from "./components/tasks/Tasks";
+import TaskPage from "./components/tasks/TaskPage";
 import "./App.css";
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/tasks" element={<Tasks />} />
+				<Route path="/tasks/:taskId" element={<TaskPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
