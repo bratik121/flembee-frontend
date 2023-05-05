0c870c8c4e5798f72fb75a03c6c28794ad8071b9
bratik121 <thiswasbrii@gmail.com>
2023-05-04 20:35:02 -0400

    añadiendo spinner

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,7 +1,9 @@
-import Input from "../../elements/Input";
 import { useInput } from "../../hooks/hooks";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Input from "../../elements/Input";
 import Button from "../../elements/Button";
+import Spinner from "../../elements/Spinner";
 import { User } from "../../types/types";
 import {
 	validateText,
@@ -17,6 +19,7 @@ function Register() {
 	const emailInput = useInput();
 	const firstNameInput = useInput();
 	const lastNameInput = useInput();
+	const [loading, setLoading] = useState(false);
 
 	const handleLogin = () => {
 		navigate("/");
@@ -51,66 +54,73 @@ function Register() {
 	return (
 		<div className="modal w-screen h-screen bg-[rgba(0,0,0,0.3)] grid items-center justify-items-center absolute top-0 right-0">
 			<div className="login w-[300px]  md:w-[800px] bg-white shadow-2xl flex flex-col px-8 md:px-20 gap-6 py-10 rounded-lg">
-				<div className="flex flex-col gap-2">
-					<h2 className="text-center text-2xl">Registrate</h2>
-				</div>
-				<div className="grid grid-cols-2 gap-8">
-					{/* Usuario */}
-					<div className="flex flex-col gap-2">
-						<Input
-							label="Usuario"
-							name="usuario"
-							error={userInput.error}
-							usernameRef={userInput.ref}
-						/>
-					</div>
-					{/* Contrasena */}
-					<div className="flex flex-col gap-2">
-						<Input
-							label="Contraseña"
-							name="password"
-							error={passwordInput.error}
-							usernameRef={passwordInput.ref}
-							password={true}
-						/>
-					</div>
-					{/* Nombre */}
-					<div className="flex flex-col gap-2">
-						<Input
-							label="Nombre"
-							name="nombre"
-							error={firstNameInput.error}
-							usernameRef={firstNameInput.ref}
-						/>
-					</div>
-					{/* Apellido */}
-					<div className="flex flex-col gap-2">
-						<Input
-							label="Apellido"
-							name="apellido"
-							error={lastNameInput.error}
-							usernameRef={lastNameInput.ref}
-						/>
-					</div>
-					{/* Email */}
-					<div className="flex flex-col gap-2 col-span-2">
-						<Input
-							label="Email"
-							name="email"
-							error={emailInput.error}
-							usernameRef={emailInput.ref}
-						/>
-					</div>
-				</div>
-				<div className="flex flex-col gap-2 items-center">
-					<Button label="Registrarse" onClick={handleClick} />
-					<span
-						className="text-blue-500 hover:text-blue-300 transition duration-300 cursor-pointer"
-						onClick={handleLogin}
-					>
-						Iniciar Sesion
-					</span>
-				</div>
+				{loading ? (
+					<Spinner />
+				) : (
+					<>
+						{" "}
+						<div className="flex flex-col gap-2">
+							<h2 className="text-center text-2xl">Registrate</h2>
+						</div>
+						<div className="grid grid-cols-2 gap-8">
+							{/* Usuario */}
+							<div className="flex flex-col gap-2">
+								<Input
+									label="Usuario"
+									name="usuario"
+									error={userInput.error}
+									usernameRef={userInput.ref}
+								/>
+							</div>
+							{/* Contrasena */}
+							<div className="flex flex-col gap-2">
+								<Input
+									label="Contraseña"
+									name="password"
+									error={passwordInput.error}
+									usernameRef={passwordInput.ref}
+									password={true}
+								/>
+							</div>
+							{/* Nombre */}
+							<div className="flex flex-col gap-2">
+								<Input
+									label="Nombre"
+									name="nombre"
+									error={firstNameInput.error}
+									usernameRef={firstNameInput.ref}
+								/>
+							</div>
+							{/* Apellido */}
+							<div className="flex flex-col gap-2">
+								<Input
+									label="Apellido"
+									name="apellido"
+									error={lastNameInput.error}
+									usernameRef={lastNameInput.ref}
+								/>
+							</div>
+							{/* Email */}
+							<div className="flex flex-col gap-2 col-span-2">
+								<Input
+									label="Email"
+									name="email"
+									error={emailInput.error}
+									usernameRef={emailInput.ref}
+								/>
+							</div>
+						</div>
+						<div className="flex flex-col gap-2 items-center">
+							<Button label="Registrarse" onClick={handleClick} />
+							<span
+								className="text-blue-500 hover:text-blue-300 transition duration-300 cursor-pointer"
+								onClick={handleLogin}
+							>
+								Iniciar Sesion
+							</span>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
---
+++ src/elements/Spinner.tsx
@@ -0,0 +1,6 @@
+import "./elements.css";
+function Spinner() {
+	return <div className="loader"></div>;
+}
+
+export default Spinner;
